1d8e3175835b14fc176ca41c8afed5c193cf8991
Shalom Habtamu <shalomhabtamum@gmail.com>
2025-02-22 15:37:13 +0300

    add error handling

--- database/db.js
+++ database/db.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
 const connectToDB = async () => {
-    await mongoose.connect(process.env.URI).then((res) => {
-        console.log("Mongodb connected succesfully.");
-    })
-}
+    await mongoose.connect(process.env.URI)
+        .then(() => {
+            console.log("Mongodb connected successfully.");
+        })
+        .catch((err) => {
+            console.error("Error connecting to MongoDB:", err);
+        });
+};
+
 export default connectToDB;
